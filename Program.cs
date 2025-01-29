a9457d2a3f27c87fbf9c7ad192e3f06497887151
Samuel <samuel@sava.be>
2025-01-29 15:38:01 +0100

    feat: version to 8.0

--- Program.cs
+++ Program.cs
@@ -1,30 +1,25 @@
-using zen_demo_dotnet.Components;
-using Aikido.Zen.DotNetCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddZenFireWall();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseZenFireWall();
+app.UseStaticFiles();
 
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
