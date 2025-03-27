eaaa89b925957036ace47769d1cdbeb02ca41478
Yannis Standaert <yannis@quickminds.be>
2025-03-27 09:42:08 +0100

    moved zen middleware to ensure it gets called after routing

--- Program.cs
+++ Program.cs
@@ -91,6 +91,11 @@ app.Use((context, next) =>
     return next();
 });
 
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.UsePublicFallback();
 // Enable Zen
 try
 {
@@ -100,11 +105,6 @@ catch(Exception e)
 {
     Console.WriteLine("Aikido does not run on ARM chips", e);
 }
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.UseRouting();
-app.UsePublicFallback();
 app.MapRazorPages();
 app.MapControllers();
 
