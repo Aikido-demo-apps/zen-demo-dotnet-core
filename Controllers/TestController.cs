8d2b2e31f78799796e28fc97b2b7689eab16a0ec
Hans Ott <hansott@hotmail.be>
2025-08-07 11:02:11 +0200

    Update to latest UI
    - Removes umami tracking code
    - Adds LLM tester

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -36,5 +36,11 @@ namespace zen_demo_dotnet.Controllers
             Request.Headers.TryGetValue("user", out var user);
             return Content($"Hello endpoint with route parameter: {id}");
         }
+
+        [HttpPost("test_llm")]
+        public IActionResult PostTestLlm()
+        {
+            return Content("Demo feature not implemented", "text/plain");
+        }
     }
 }
